6c12edfe74d302a768e765301f90902be5a32665
Vadim Uvarov <razoru93@gmail.com>
2016-01-14 19:52:26 +0600

    Refactoring

--- DiscreteHMM.py
+++ DiscreteHMM.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy import stats
 import copy
 
-class HiddenMarkovModel:
+class DHMM:
     
     """Implementation of discrete observations Hidden Markov Model.
        Observations are integers from 0 to M-1
@@ -21,34 +21,34 @@ class HiddenMarkovModel:
         """
         if seed is not None:
             np.random.seed(seed)        
-        self.n = n
-        self.m = m
+        self._n = n
+        self._m = m
         # if parameters are not defined - give them some statistically correct
         # default values
         if pi is not None:
-            self.pi = np.array(pi)
+            self._pi = np.array(pi)
         elif seed is None:
-            self.pi = np.full(n, 1.0/n)
+            self._pi = np.full(n, 1.0/n)
         else:
-            self.pi = generate_discrete_distribution(n)
+            self._pi = _generate_discrete_distribution(n)
         # transition matrix
         if a is not None:
-            self.a = np.array(a)
+            self._a = np.array(a)
         elif seed is None:
-            self.a = np.full((n, n), 1.0/n)
+            self._a = np.full((n, n), 1.0/n)
         else:
-            self.a = np.empty(shape=(n, n))
+            self._a = np.empty(shape=(n, n))
             for i in range(n):
-                self.a[i, :] = generate_discrete_distribution(n)          
+                self._a[i, :] = _generate_discrete_distribution(n)          
         # emission matrix
         if b is not None:
-            self.b = np.array(b)
+            self._b = np.array(b)
         elif seed is None:
-            self.b = np.full((n, m), 1.0/m)
+            self._b = np.full((n, m), 1.0/m)
         else:
-            self.b = np.empty(shape=(n, m))
+            self._b = np.empty(shape=(n, m))
             for i in range(n):
-                self.b[i, :] = generate_discrete_distribution(m)
+                self._b[i, :] = _generate_discrete_distribution(m)
     
     def generate_sequence(self, T, seed=None):
         """
@@ -72,46 +72,18 @@ class HiddenMarkovModel:
         # prepare array for sequence
         seq = np.empty(T, dtype=int)
         states = np.empty(T, dtype=int)
-        
-        """
-        # discrete distrubution of initial states
-        pi_distr = \
-            stats.rv_discrete(values = (np.arange(self.n), self.pi))
-        # set discrete distribution of transtition probabilities for each state
-        a_distr_list = []
-        for i in range(self.n):
-            temp = stats.rv_discrete(values=(np.arange(self.n),(self.a[i,:])))
-            a_distr_list.append(temp)
-        # set discrete distribution of symbols for each state
-        b_distr_list = []
-        for i in range(self.n):
-            temp = stats.rv_discrete(values=(np.arange(self.m),(self.b[i,:])))
-            b_distr_list.append(temp)
         # generation
-        # generate number of initial state
-        state = pi_distr.rvs()
-        print state
-        # generate observation from the state
-        seq[0] = b_distr_list[state].rvs()
-        for t in range(1, T):
-            # transit to a new state
-            state = a_distr_list[state].rvs()
-            print state
-            # generate observation from the state
-            seq[t] = b_distr_list[state].rvs()
-        """
-        
-        state = _get_sample_discrete_distr(self.pi)
+        state = _get_sample_discrete_distr(self._pi)
         states[0] = state
-        seq[0] = _get_sample_discrete_distr(self.b[state,:])
+        seq[0] = _get_sample_discrete_distr(self._b[state,:])
         for t in range(1, T):
-            state = _get_sample_discrete_distr(self.a[state,:])
+            state = _get_sample_discrete_distr(self._a[state,:])
             states[t] = state
-            seq[t] = _get_sample_discrete_distr(self.b[state,:])
+            seq[t] = _get_sample_discrete_distr(self._b[state,:])
             
         return seq, states
     
-    def calc_forward_noscaling(self, seq):
+    def _calc_forward_noscale(self, seq):
         """ calculate forward variables (no scaling)
         seq -- sequence of observations (1d array)
         return(1) -- likelihood of sequence being produced by model
@@ -119,18 +91,18 @@ class HiddenMarkovModel:
         """        
         T = seq.size
         # initialization step:
-        alpha = np.empty((T, self.n))
-        alpha[0,:] = self.pi[:] * self.b[:,seq[0]]
+        alpha = np.empty((T, self._n))
+        alpha[0,:] = self._pi[:] * self._b[:,seq[0]]
         # induction step:
         for t in range(T-1):
-            for i in range(self.n):
+            for i in range(self._n):
                 alpha[t+1,i] = \
-                    self.b[i,seq[t+1]] * np.sum(alpha[t,:]*self.a[:,i])
+                    self._b[i,seq[t+1]] * np.sum(alpha[t,:]*self._a[:,i])
         # termination:
         likelihood = np.sum(alpha[-1,:])
         return likelihood, alpha[:,:]
     
-    def calc_forward_logsumexp(self, seq):
+    def _calc_forward_logsumexp(self, seq):
         # TODO: needs to be thought through more carefully
         """ calculate forward variables (log-sum-exp trick)
         seq -- sequence of observations (1d array)
@@ -139,15 +111,15 @@ class HiddenMarkovModel:
         """        
         T = seq.size
         # initialization step:
-        log_alpha = np.empty((T, self.n))
-        log_alpha[0, :] = np.log(self.pi[:] * self.b[:,seq[0]])
+        log_alpha = np.empty((T, self._n))
+        log_alpha[0, :] = np.log(self._pi[:] * self._b[:,seq[0]])
         # induction step:
         for t in range(T-1):
             # for each state calc forward variable
-            for i in range(self.n):
+            for i in range(self._n):
                 # calc values under exponent
-                log_temps = np.log(self.b[:,seq[t+1]]) + \
-                    np.log(self.a[:,i]) + log_alpha[t,:]
+                log_temps = np.log(self._b[:,seq[t+1]]) + \
+                    np.log(self._a[:,i]) + log_alpha[t,:]
                 max_log_temp = np.max(log_temps)
                 # apply log-sum-exp trick
                 log_alpha[t+1,i] = max_log_temp + \
@@ -157,7 +129,7 @@ class HiddenMarkovModel:
         likelihood = np.sum(alpha[T-1,:])
         return likelihood, alpha
     
-    def calc_forward_scaled(self, seq):
+    def _calc_forward_scaled(self, seq):
         """ calculate forward variables (scaled)
         seq -- sequence of observations, array(T)
         return(1) -- loglikelihood of sequence being produced by model
@@ -165,19 +137,19 @@ class HiddenMarkovModel:
         return(3) -- c: array(T) of scaling coefficients
         """
         T = seq.size
-        sc_alpha = np.empty(shape=(T, self.n))
-        alpha_pr = np.empty(self.n) # from previous step
-        alpha = np.empty(self.n)
+        sc_alpha = np.empty(shape=(T, self._n))
+        alpha_pr = np.empty(self._n) # from previous step
+        alpha = np.empty(self._n)
         c = np.empty(T)
         # initialization
-        alpha_pr[:] = self.pi[:] * self.b[:,seq[0]]
+        alpha_pr[:] = self._pi[:] * self._b[:,seq[0]]
         c[0] = 1.0 / np.sum(alpha_pr[:])
         sc_alpha[0,:] = alpha_pr[:] * c[0]
         # induction
         for t in range(T-1):
-            for i in range(self.n):
+            for i in range(self._n):
                 alpha[i] = \
-                    self.b[i,seq[t+1]] * np.sum(sc_alpha[t,:]*self.a[:,i])
+                    self._b[i,seq[t+1]] * np.sum(sc_alpha[t,:]*self._a[:,i])
             c[t+1] = 1.0 / np.sum(alpha[:])
             sc_alpha[t+1,:] = c[t+1] * alpha[:]
             alpha_pr = np.array(alpha)
@@ -185,48 +157,48 @@ class HiddenMarkovModel:
         loglikelihood = -np.sum(np.log(c[:]))
         return loglikelihood, sc_alpha[:,:], c[:]
     
-    def calc_backward_noscaling(self, seq):
+    def _calc_backward_noscale(self, seq):
         """ Calc backward variables given the model and sequence
         seq -- sequence of observations, array(T)
         return -- beta: array(T, n) of backward variables
         """
         T = seq.size
-        beta = np.empty(shape=(T, self.n))
+        beta = np.empty(shape=(T, self._n))
         # initialization
         beta[-1, :] = 1.0
         # induction
         for t in reversed(range(T-1)):
-            for i in range(self.n):
+            for i in range(self._n):
                 beta[t,i] = \
-                    np.sum(self.a[i,:] * self.b[:,seq[t+1]] * beta[t+1,:])
+                    np.sum(self._a[i,:] * self._b[:,seq[t+1]] * beta[t+1,:])
         # TODO: return also the likelihood  
-        #print "beta" + str(np.sum(beta[0,:]*self.b[:,seq[0]]))
+        #print "beta" + str(np.sum(beta[0,:]*self._b[:,seq[0]]))
         return beta[:,:]
         
-    def calc_backward_scaled(self, seq, c):
+    def _calc_backward_scaled(self, seq, c):
         """ Calc backward variables using standard scaling procedure
         seq -- sequence of observations, array(T)
         c -- array(T) of scaling coefficients
         return -- sc_beta: array(T, n) of scaled backward variables
         """
         T = seq.size
-        sc_beta = np.empty(shape=(T, self.n))
-        beta_pr = np.empty(self.n) # from previous step
-        beta = np.empty(self.n)
+        sc_beta = np.empty(shape=(T, self._n))
+        beta_pr = np.empty(self._n) # from previous step
+        beta = np.empty(self._n)
         # initialization
         beta_pr[:] = 1.0
         sc_beta[-1, :] = c[-1] * beta_pr[:]
         # induction
         for t in reversed(range(T-1)):
-            for i in range(self.n):
+            for i in range(self._n):
                 beta[i] = \
-                    np.sum(self.a[i,:] * self.b[:,seq[t+1]]  * sc_beta[t+1,:])
+                    np.sum(self._a[i,:] * self._b[:,seq[t+1]]  * sc_beta[t+1,:])
             sc_beta[t, :] = c[t] * beta[:]
             beta_pr = np.array(beta)
         # TODO: return also the likelihood
         return sc_beta
     
-    def calc_xi_noscaling(self, seq, alpha, beta, p):
+    def _calc_xi_noscale(self, seq, alpha, beta, p):
         """ Calc xi(t,i,j), t=1..T, i,j=1..N - array of probabilities of
         being in state i and go to state j in time t given the model and seq
         seq -- sequence of observations, array(T)
@@ -236,15 +208,15 @@ class HiddenMarkovModel:
         return - xi, array(T,N,N)
         """
         T = seq.size
-        xi = np.empty(shape=(T-1, self.n, self.n))
+        xi = np.empty(shape=(T-1, self._n, self._n))
         for t in range(T-1):
-            for i in range(self.n):
+            for i in range(self._n):
                 xi[t,i,:] = \
-                    alpha[t,i] * self.a[i,:] * self.b[:,seq[t+1]] * beta[t+1,:]
+                    alpha[t,i] * self._a[i,:] * self._b[:,seq[t+1]] * beta[t+1,:]
         xi[:,:,:] /= p
         return xi
     
-    def calc_gamma_noscaling(self, alpha, beta, p, xi=None):
+    def _calc_gamma_noscale(self, alpha, beta, p, xi=None):
         """ Calc gamma(t,i), t=1..T, i=1..N -- array of probabilities of
         being in state i at the time t given the model and sequence
         mode 1:
@@ -255,7 +227,7 @@ class HiddenMarkovModel:
         xi -- array of xi values (refer to calc_xi_noscaling)
         """
         T = alpha.shape[0]
-        gamma = np.empty(shape=(T,self.n))
+        gamma = np.empty(shape=(T,self._n))
         if xi is not None:
             gamma[:-1, :] = np.sum(xi[:,:,:], axis=2)
             gamma[-1, :] = alpha[-1, :] * beta[-1, :] / p
@@ -263,57 +235,50 @@ class HiddenMarkovModel:
             gamma[:, :] = alpha[:, :] * beta[:, :] / p
         return gamma[:, :]
         
-def train_hmm_baumwelch_noscaling(seq, hmm0, rtol=0.1, max_iter=10):
-    """ Train a HMM given a training sequence & an initial approximation 
-    seq -- training sequence
-    hmm0 -- initial approximation of hmm parameters
-    rtol -- relative tolerance for termination of iteration process
-    max_iter -- max number of iterations
-    """
-    T = seq.size
-    hmm = copy.deepcopy(hmm0)
-    iteration = 0
-    # TODO: maybe p_prev calc here and change E and M step in iteration?
-    p_prev = -100.0 # likelihood on previous iteration (dummy value)
-    p = 100.0       # likelihood on cur iteration (dummy value)
-    #while not np.isclose(p_prev,p,atol=eps) and iteration < max_iter:
-    #while np.abs(p_prev-p)/p > rtol and iteration < max_iter:
-    #while np.abs(p_prev-p) > rtol and iteration < max_iter:
-    while iteration < max_iter:
-        #print np.abs(p_prev-p)/p
-        p_prev = p
-        p, alpha = hmm.calc_forward_noscaling(seq)
-        #print p
-        #if iteration == 0:
-        #    print "alpha"
-        #    print alpha
-        beta = hmm.calc_backward_noscaling(seq)
-        xi = hmm.calc_xi_noscaling(seq, alpha, beta, p)
-        gamma = hmm.calc_gamma_noscaling(alpha, beta, p, xi)
-        # re-estimation
-        hmm.pi[:] = gamma[0,:]
-        for i in range(hmm.n):
-            for j in range(hmm.n):
-                hmm.a[i,j] = np.sum(xi[:,i,j]) / np.sum(gamma[:-1,i]) 
-        for i in range(hmm.n):
-            for m in range(hmm.m):
-                gamma_sum = 0.0
-                for t in range(T):
-                    if seq[t] == m:
-                        gamma_sum += gamma[t,i]
-                hmm.b[i,m] = gamma_sum / np.sum(gamma[:,i])
-        iteration += 1
-    print "train_hmm_baumwelch_noscaling: iteration = " + str(iteration)
-    return hmm
+    def train_baumwelch_noscale(self, seq, rtol, max_iter):
+        """ Train a HMM given a training sequence & an initial approximation 
+        initial approximation is taken from class parameters
+        seq -- training sequence
+        rtol -- relative tolerance for termination of iteration process
+        max_iter -- max number of iterations
+        """
+        T = seq.size
+        iteration = 0
+        p_prev = -100.0 # likelihood on previous iteration
+        p = 100.0       # likelihood on cur iteration
+        # TODO: if next likelihood is lower than previous - 
+        # TODO: then take previous parameters ?
+        while np.abs(p_prev-p)/p > rtol and iteration < max_iter:
+            p_prev = p
+            p, alpha = self._calc_forward_noscaling(seq)
+            beta = self._calc_backward_noscaling(seq)
+            xi = self._calc_xi_noscaling(seq, alpha, beta, p)
+            gamma = self._calc_gamma_noscaling(alpha, beta, p, xi)
+            # re-estimation
+            self._pi[:] = gamma[0,:]
+            for i in range(self._n):
+                for j in range(self._n):
+                    self._a[i,j] = np.sum(xi[:,i,j]) / np.sum(gamma[:-1,i]) 
+            for i in range(self._n):
+                for m in range(self._m):
+                    gamma_sum = 0.0
+                    for t in range(T):
+                        if seq[t] == m:
+                            gamma_sum += gamma[t,i]
+                    self._b[i,m] = gamma_sum / np.sum(gamma[:,i])
+            iteration += 1
+        likelihood, _ = self._calc_forward_noscaling(seq)
+        return likelihood, iteration
     
-def choose_best_hmm_using_bauwelch(seq, train_func, hmms0_size, n, m,
-                                   hmms0=None, rtol=0.1, max_iter=10):
+def choose_best_hmm_using_bauwelch(seq, hmms0_size, n, m, isScale = False,
+                                   hmms0=None, rtol=0.1, max_iter=10,
+                                   verbose=False):
     """ Train several hmms using baumwelch algorithm and choose the best one
-    seq -- training sequence
-    train_func -- Baum-Welch algorithm training function
+    seq -- training sequence 
     hmms0_size -- number of initial approximations
     n -- number of HMM states
     m -- number of HMM symbols
+    isScale -- is scaling needed
     mode1: hmms0 -- array of initial approximations
     mode2: hmms0 -- None -- will be generated randomly
     rtol -- relative tolerance (stopping criterion)
@@ -322,24 +287,30 @@ def choose_best_hmm_using_bauwelch(seq, train_func, hmms0_size, n, m,
     # generate approximations if not given any
     if hmms0 is None:
         hmms0 = []
-        seeds = np.random.randint(1, high=np.iinfo(np.int32).max, size=hmms0_size) # seeds
+        # seeds for hmm generation from 1 to maximum int32
+        seeds = \
+            np.random.randint(1, high=np.iinfo(np.int32).max, size=hmms0_size)
         for seed in seeds:
-            hmms0.append(HiddenMarkovModel(n, m, seed=seed))
+            hmms0.append(DHMM(n, m, seed=seed))
     # calc and choose the best hmm estimate
-    p_max = np.finfo(np.float64).min
+    p_max = np.finfo(np.float64).min # minimal value possible
     for hmm0 in hmms0:
-        hmm = train_func(seq, hmm0, rtol, max_iter)
-        p, _ = hmm.calc_forward_noscaling(seq)
+        # TODO: scaled baum and ternary operator
+        if not isScale:
+            p, iteration = hmm0.train_baumwelch_noscale(seq, rtol, max_iter)
+        else:
+            raise NotImplementedError, "Scaled baum-welch is not impl. yet"
         if (p_max < p):
-            hmm_best = copy.deepcopy(hmm)
+            hmm_best = copy.deepcopy(hmm0)
             p_max = p
-        #print "another approximation: p=" + str(p)
-        #print hmm0.pi
-        #print hmm0.a
-        #print hmm0.b
+        if verbose:
+            print "another approximation: p=" + str(p)
+            print hmm0.pi
+            print hmm0.a
+            print hmm0.b
     return hmm_best
 
-def generate_discrete_distribution(n):
+def _generate_discrete_distribution(n):
     """ Generate n values > 0.0, whose sum equals 1.0
     """
     xs = np.array(stats.uniform.rvs(size=n))
