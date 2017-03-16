134bc4c8ba98d3aa9bfb7bcf391809e164224587
Vadim Uvarov <razoru93@gmail.com>
2017-03-16 18:59:19 +0700

    Now each experiment is saved in a separate unique folder along with its parameters. Auto-variable name deducer.

--- research_ghmm_class_decode_imp.py
+++ research_ghmm_class_decode_imp.py
@@ -6,13 +6,14 @@ from scipy.stats import randint as sp_randint
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import time
+import datetime
 import GaussianHMM as ghmm
 import winsound
 import sklearn.svm
 import sklearn.cross_validation
 import sklearn.grid_search
 import sklearn.preprocessing
+import os
 from research import make_missing_values
 from research import gen_gaps_positions
 from research import evaluate_classification
@@ -20,13 +21,25 @@ from research import evaluate_decoding_imputation
 from research import make_svm_training_seqs
 import HMM
 
+def get_var_name(var, used_names, var_list):
+    all_variables = globals()
+    for key in all_variables:
+        if all_variables[key] is var and key not in used_names:
+            return key
+            
+def save_vars_to_file(f, var_list):
+    used_names = []
+    for var in var_list:
+        name = get_var_name(var, used_names, var_list)
+        used_names.append(name)
+        f.write("{} = {}\n".format(name, var))
+
 #
 # Experiment parameters
 #
 # experiment settings
 n_of_launches = 50
 is_verbose = False
-out_dir = "out/"
 # derivatives calculation settings
 wrt = ['a']
 # missing data settings
@@ -34,8 +47,9 @@ wrt = ['a']
 n_of_gaps_train_svm = "make_svm_training_seqs({},100,10)".format(n_of_gaps_train_hmm)
 #n_of_gaps_train_svm = n_of_gaps_train_hmm
 #n_of_gaps_train_svm = (n_of_gaps_train_hmm, 90)
-gaps_range = list(range(0, 100, 10))
-is_gaps_places_different = True
+class_gaps_range = list(range(0, 100, 10))
+is_train_gaps_places_different = True
+is_class_gaps_places_different = True
 seed_gen_train_gaps = 222
 # hmm training settings
 rtol = 1e-5
@@ -47,6 +61,9 @@ seed_gen_train_seqs = 565
 # size of training and classification samples
 T = 100
 K = 100
+# size of sequences to calc symmetric distances between hmms
+T_dist = 500
+K_dist = 100
 # standard imputation settings
 n_neighbours = 10
 # SVM hyperparams validation settings
@@ -55,7 +72,7 @@ class pow_ten_uniform():
         self.gen = sp_uniform(a, b)
     def rvs(self):
         return 10.0 ** self.gen.rvs()
-n_trials = 10000   # randomized trials of SVM hypermarameters
+n_trials_svm_random_search = 1000   # randomized trials of SVM hypermarameters
 cv_n_folds = 4
 kernels=['poly', 'rbf']
 C_range = (-3., 10.)
@@ -96,11 +113,6 @@ for n in range(N):
         sig[n,m,:,:] = np.eye(Z) * sig_val
 hmm1_orig = ghmm.GHMM(N, M, Z, mu, sig, pi=pi, a=a, tau=tau)
 
-filename = "ghmm_dA={}_sigval={}_N={}_M={}_Z={}_K={}_T={}_n_gaps_train={}"\
-    "_launches={}_n_gaps_train_svm={}_svm_n_trials={}_hmms0_size={}"\
-    .format(dA, sig_val, N, M, Z, K, T, n_of_gaps_train_hmm, n_of_launches,
-            n_of_gaps_train_svm, n_trials, hmms0_size)
-
 # hmm 2
 pi = np.array([0.3, 0.4, 0.3])
 a = np.array([[0.1+dA, 0.7-dA, 0.2],
@@ -122,22 +134,25 @@ for n in range(N):
         sig[n,m,:,:] = np.eye(Z) * sig_val
 hmm2_orig = ghmm.GHMM(N, M, Z, mu, sig, pi=pi, a=a, tau=tau)
 
+
+
 #
 # Research
 #
-start_time = time.time()
+
 print("n_of_gaps_train_hmm = {}".format(n_of_gaps_train_hmm))
 #
 # Train HMMs
 #
 print("training hmms")
+start_time = datetime.datetime.now()
 train_seqs_orig1, _ = hmm1_orig.generate_sequences(K, T, seed=seed_gen_train_seqs)
 train_seqs_orig2, _ = hmm2_orig.generate_sequences(K, T, seed=seed_gen_train_seqs)
 
 # generate gaps posititons in the training sequences
 np.random.seed(seed_gen_train_gaps)
-to_dissapears1 = gen_gaps_positions(K, T, is_gaps_places_different)
-to_dissapears2 = gen_gaps_positions(K, T, is_gaps_places_different)
+to_dissapears1 = gen_gaps_positions(K, T, is_train_gaps_places_different)
+to_dissapears2 = gen_gaps_positions(K, T, is_train_gaps_places_different)
 # mark some elements as missing
 # hmm 1
 train_seqs1, train_avails1 = make_missing_values(train_seqs_orig1, to_dissapears1,
@@ -159,70 +174,86 @@ hmm2, _, iter2, n_of_best2 = ghmm.train_best_hmm_baumwelch(
 print("hmm training completed, iter={}/{}".format(iter1, iter2))
 if is_verbose:
     print("hmms:\nhmm1={}\nhmm2={}".format(hmm1, hmm2))
-distance = HMM.calc_symmetric_distance(hmm1, hmm2, 500, 100)
-print("distance between hmms = {}".format(distance))
-print("--- {} minutes ---".format((time.time()-start_time) / 60))
+distance_bw_hmm1_hmm2 = HMM.calc_symmetric_distance(hmm1, hmm2, T_dist, K_dist)
+print("distance between hmms = {}".format(distance_bw_hmm1_hmm2))
+print("---Elapsed: {} ---".format((datetime.datetime.now() - start_time) / 60))
 
 #
-# train SVM classifier
+# Prepare training sequences for SVM classifier
 #
-# mark some elements as missing with varying percent of gaps between sequences
+print("Preparing training sequences for SVM classifier")
+start_time = datetime.datetime.now()
 if n_of_gaps_train_svm == (n_of_gaps_train_hmm, 90):
+    # mark some elements as missing with varying percent of gaps between sequences
     train_svm_seqs1, train_svm_avails1 = make_missing_values(train_seqs_orig1, to_dissapears1,
                                                              n_of_gaps_train_svm)
     train_svm_seqs2, train_svm_avails2 = make_missing_values(train_seqs_orig2, to_dissapears2,
                                                              n_of_gaps_train_svm)
-# SVM train seqs are formed using make_svm_training_seqs procedure
+
 elif n_of_gaps_train_svm == "make_svm_training_seqs({},100,10)".format(n_of_gaps_train_hmm):
+    # SVM train seqs are formed using make_svm_training_seqs procedure
     train_svm_seqs1, train_svm_avails1 = make_svm_training_seqs(train_seqs_orig1,
                                                             to_dissapears1,
                                                             range(n_of_gaps_train_hmm, 100, 10))
     train_svm_seqs2, train_svm_avails2 = make_svm_training_seqs(train_seqs_orig2,
                                                             to_dissapears2,
                                                             range(n_of_gaps_train_hmm, 100, 10))
-# SVM train seqs are the same as HMM train seqs
 elif n_of_gaps_train_svm == n_of_gaps_train_hmm:
+    # SVM train seqs are the same as HMM train seqs
     train_svm_seqs1 = train_seqs1
     train_svm_seqs2 = train_seqs2
     train_svm_avails1 = train_avails1
     train_svm_avails2 = train_avails2
 else:
     raise Exception("Wrong n_of_gaps_train_svm!")
-
-#
-# Optimize SVM hyperparams
-#
-print("Performing cross-validation of SVM params")
-start_time = time.time()
-clf = sklearn.svm.SVC(cache_size=500, max_iter=10000)
+# precalc derivatives here
 X, y = ghmm._form_train_data_for_SVM([hmm1, hmm2],
                                      [train_svm_seqs1, train_svm_seqs2],
                                      [train_svm_avails1, train_svm_avails2],
                                      wrt)
 scaler = sklearn.preprocessing.StandardScaler()
 X = scaler.fit_transform(X)
+print("---Elapsed: {} ---".format((datetime.datetime.now() - start_time) / 60))
+
+#
+# Optimize SVM hyperparams and train SVM classifier
+#
+print("Performing cross-validation of SVM params")
+start_time = datetime.datetime.now()
+clf = sklearn.svm.SVC(cache_size=500, max_iter=10000)
 kfold = sklearn.cross_validation.KFold(len(X), n_folds=cv_n_folds, shuffle=True, random_state=cv_random_state)
-clf = sklearn.grid_search.RandomizedSearchCV(clf, param_distributions, n_iter=n_trials,
-                                                       cv=kfold, random_state=cv_random_state)
+clf = sklearn.grid_search.RandomizedSearchCV(clf, param_distributions, n_iter=n_trials_svm_random_search,
+                                             cv=kfold, random_state=cv_random_state)
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     clf.fit(X, y)
 print("best score on SVM model on training set: {}".format(clf.best_score_))
-print("--- {} minutes ---".format((time.time()-start_time) / 60))
+print("---Elapsed: {} ---".format((datetime.datetime.now() - start_time) / 60))
+
 
 #
-# train final SVM classifier using the best hyperparams
+# Save experiment params to file
 #
-#print("training SVM classifier")
-#clf, scaler = ghmm.train_svm_classifier([hmm1, hmm2],
-#                                        [train_svm_seqs1, train_svm_seqs2],
-#                                        svm_params, [train_svm_avails1, train_svm_avails2],
-#                                        wrt=wrt)
+str_cur_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+out_dir = "out/research_ghmm_class_decode_imp_{}/".format(str_cur_time)
+os.mkdir(out_dir)
+f = open(out_dir+"experiment_params.txt", "w")
+save_vars_to_file(f, [n_of_launches, wrt, n_of_gaps_train_svm, class_gaps_range,
+                      is_train_gaps_places_different, is_class_gaps_places_different,
+                      seed_gen_train_gaps, rtol, max_iter, hmms0_size, seed_training,
+                      seed_gen_train_seqs, T, K, T_dist, K_dist, n_neighbours, n_trials_svm_random_search,
+                      cv_n_folds, kernels, C_range, gamma_range, coef0_range, degree_range,
+                      cv_random_state, dA, sig_val, hmm1_orig, hmm2_orig,
+                      hmm1, hmm2, iter1, iter2, n_of_best1, n_of_best2,
+                      distance_bw_hmm1_hmm2])
+f.write("clf.estimator = {}\n".format(clf.best_estimator_))
+f.close()
+
 
 #
 # initialize variables to store experiment data
 #
-xs = np.array(gaps_range, dtype=np.int)
+xs = np.array(class_gaps_range, dtype=np.int)
 
 class_percents_mlc_marg = np.zeros_like(xs, dtype=np.float)
 class_percents_mlc_viterbi = np.zeros_like(class_percents_mlc_marg)
@@ -244,6 +275,8 @@ imputed_sum_squares_diff_mean = np.zeros_like(class_percents_mlc_marg)
 
 
 # make several launches on different seeds to get the average result
+print("Starting the experiment")
+start_time = datetime.datetime.now()
 for n_of_launch in range(n_of_launches):
     print("n_of_launch = {}".format(n_of_launch))
     # generate sequences to classify
@@ -252,12 +285,12 @@ for n_of_launch in range(n_of_launches):
 
     # generate gaps posititons in the sequences
     np.random.seed(n_of_launch)
-    to_dissapears1 = gen_gaps_positions(K, T, is_gaps_places_different)
-    to_dissapears2 = gen_gaps_positions(K, T, is_gaps_places_different)
+    to_dissapears1 = gen_gaps_positions(K, T, is_class_gaps_places_different)
+    to_dissapears2 = gen_gaps_positions(K, T, is_class_gaps_places_different)
 
     # the experiment
     step = 0
-    for n_of_gaps in gaps_range:
+    for n_of_gaps in class_gaps_range:
         if is_verbose:
             print("n_of_gaps = {}".format(n_of_gaps))
         # mark some elements as missing
@@ -367,10 +400,10 @@ for n_of_launch in range(n_of_launches):
 #                                     seqs1, states_list1, seqs_orig1, avails1,
 #                                     algorithm_gaps='mean', n_neighbours=n_neighbours)
         if is_verbose:
-            print("--- {} minutes ---".format((time.time()-start_time) / 60))
+            print("---Elapsed: {} ---".format((datetime.datetime.now() - start_time) / 60))
             print()
         step += 1
-    print("--- {} minutes ---".format((time.time()-start_time) / 60))
+    print("---Elapsed: {} ---".format((datetime.datetime.now() - start_time) / 60))
     print()
 #
 # get the average values
@@ -427,7 +460,7 @@ plt.plot(xs, class_percents_svm_marg, '-', label=u"SVM, Маргинализац
 #plt.plot(xs, class_percents_svm_mean, '-.', dash_capstyle='round', lw=2.0, label=u"SVM, Среднее")
 plt.legend()
 plt.show()
-plt.savefig(out_dir + "/class_" + filename + ".png")
+plt.savefig(out_dir + "classification.png")
 plt.close()
 
 #plt.figure(2)
@@ -444,7 +477,7 @@ plt.close()
 #plt.plot(xs, decoded_percents_mean, '-.', dash_capstyle='round', lw=2.0, label=u"Среднее")
 #plt.legend()
 #plt.show()
-#plt.savefig(out_dir + "/decode_" + filename + ".png")
+#plt.savefig(out_dir + "decoding.png")
 #
 #plt.figure(3)
 #suptitle = u"Исследование алгоритмов восстановленных последовательностей с пропусками "\
@@ -460,21 +493,26 @@ plt.close()
 #plt.plot(xs, imputed_sum_squares_diff_mean, '-.', dash_capstyle='round', lw=2.0, label=u"Среднее")
 #plt.legend()
 #plt.show()
-#plt.savefig(out_dir + "/impute_" + filename + ".png")
+#plt.savefig(out_dir + "imputation.png")
 
+to_file = np.asarray([xs, class_percents_mlc_marg, class_percents_mlc_viterbi,
+                      class_percents_mlc_gluing, class_percents_mlc_mean,
+                      class_percents_mlc_viterbi1, class_percents_mlc_viterbi2,
+                      class_percents_svm_marg, class_percents_svm_viterbi,
+                      class_percents_svm_gluing, class_percents_svm_mean,
+                      decoded_percents_viterbi, decoded_percents_mean,
+                      imputed_sum_squares_diff_viterbi, imputed_sum_squares_diff_mean])
+np.savetxt(out_dir + "class_decode_impute.csv", to_file.T, delimiter=',',
+           header="xs,class_percents_mlc_marg,class_percents_mlc_viterbi,"
+                  "class_percents_mlc_gluing,class_percents_mlc_mean,"
+                  "class_percents_mlc_viterbi1,class_percents_mlc_viterbi2,"
+                  "class_percents_svm_marg,class_percents_svm_viterbi,"
+                  "class_percents_svm_gluing,class_percents_svm_mean,"
+                  "decoded_percents_viterbi,decoded_percents_mean,"
+                  "imputed_sum_squares_diff_viterbi,imputed_sum_squares_diff_mean")
 
-#to_file = np.asarray([xs, class_percents_marg, class_percents_gluing,
-#                      class_percents_viterbi, class_percents_mean,
-#                      decoded_percents_viterbi, decoded_percents_mean,
-#                      imputed_percents_viterbi, imputed_percents_mean])
-#np.savetxt(out_dir + "/class_decode_impute_" + filename + ".csv", to_file.T, delimiter=';',
-#           header="xs;class_percents_marg;class_percents_gluing;"
-#                  "class_percents_viterbi;class_percents_mean;"
-#                  "decoded_percents_viterbi;decoded_percents_mean;"
-#                  "imputed_percents_viterbi;imputed_percents_mean")
-#
-#print("--- {} minutes ---".format((time.time()-start_time) / 60))
 
 #filename = "out/dhmm_classification_dA0.4_T100_K100_x100"
 #xs, class_percents_marg, class_percents_gluing, class_percents_viterbi, class_percents_mean\
 #    = np.loadtxt(filename+".csv", delimiter=';', unpack=True)
+
